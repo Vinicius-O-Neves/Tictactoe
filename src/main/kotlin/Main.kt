f6396ffc9ebf6b537af366d4419fc8328c1050f0
Vinicius Neves <vinicius.neeves@outlook.com>
2025-10-17 10:25:07 -0300

    feat: fixed code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,6 @@ package com.br.tictactoe
 
 import com.br.tictactoe.di.injectGameModules
 import com.br.tictactoe.domain.GameController
-import com.br.tictactoe.domain.models.Game
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
@@ -11,27 +10,37 @@ import io.ktor.server.websocket.*
 import org.koin.ktor.ext.inject
 import org.koin.ktor.plugin.Koin
 import java.time.Duration
-import java.util.concurrent.ConcurrentHashMap
+
+class Main {
+
+    init {
+        run()
+    }
+
+    private fun run() {
+        val port = System.getenv("PORT")?.toInt() ?: 8080
+
+        embeddedServer(Netty, port = port, host = "0.0.0.0") {
+            install(WebSockets) {
+                pingPeriod = Duration.ofSeconds(15)
+                timeout = Duration.ofSeconds(30)
+            }
+
+            install(Koin) {
+                modules(injectGameModules())
+            }
+
+            routing {
+                val controller: GameController by inject()
+
+                webSocket("/tictactoe") {
+                    controller.handleConnection(session = this)
+                }
+            }
+        }.start(wait = true)
+    }
+}
 
 fun main() {
-    val port = System.getenv("PORT")?.toInt() ?: 8080
-
-    embeddedServer(Netty, port = port, host = "0.0.0.0") {
-        install(WebSockets) {
-            pingPeriod = Duration.ofSeconds(15)
-            timeout = Duration.ofSeconds(30)
-        }
-
-        install(Koin) {
-            modules(injectGameModules())
-        }
-
-        routing {
-            val controller: GameController by inject()
-
-            webSocket("/tictactoe") {
-                controller.handleConnection(session = this)
-            }
-        }
-    }.start(wait = true)
+    Main()
 }
